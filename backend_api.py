9a6cd10380ca31e9654258695822b08a960019ca
jeorgeiiii <princemehra3666@gmail.com>
2026-04-13 12:59:48 +0530

    fix CORS

--- backend_api.py
+++ backend_api.py
@@ -497,12 +497,11 @@ app = FastAPI(title="SGSITS Timetable API", version="4.0.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080", "http://localhost:5173", "http://localhost:3000", "*"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 security = HTTPBearer()
 
 
